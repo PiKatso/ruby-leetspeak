ce71708ed1eaa854f5643b35915f95fc9b11cb7f
Jennifer Kinsey and Kat Gifford <jennifer@>
2017-04-17 12:01:55 -0700

    add second test for o to turn to 0

--- leetspeak/lib/leetspeak.rb
+++ leetspeak/lib/leetspeak.rb
@@ -3,8 +3,10 @@ class String
     new_arr = []
     input_arr= self.split("")
     input_arr.each() do |char|
-      if char.==("e")
+      if char.==("e") or char.==("E")
         new_arr.push("3")
+      elsif char.==("o")or char.==("O")
+        new_arr.push("0")
       else
         new_arr.push(char)
       end
--- leetspeak/spec/leetspeak_spec.rb
+++ leetspeak/spec/leetspeak_spec.rb
@@ -5,4 +5,7 @@ describe('String#leetspeak')do
   it("The letter 'e' should be replaced with 3.") do
     expect(("streng").leetspeak()).to(eq("str3ng"))
   end
+  it("The letter 'o' should be replaced with 0.") do
+    expect(("strong").leetspeak()).to(eq("str0ng"))
+  end
 end
