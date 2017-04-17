4990c45519d8425dc24010fb3092b8a3b4d5e6cc
Jennifer Kinsey and Kat Gifford <kat@>
2017-04-17 14:57:12 -0700

    working state for leetspeak. need to workout subsequent words begining with s to stay at s instead of z.

--- leetspeak/lib/leetspeak.rb
+++ leetspeak/lib/leetspeak.rb
@@ -2,11 +2,24 @@ class String
   define_method(:leetspeak) do
     new_arr = []
     input_arr= self.split("")
-    input_arr.each() do |char|
+
+    if input_arr.at(0) ==("s") or input_arr.at(0)==("S")
+      input_arr.shift()
+      input_arr.unshift("~")
+    end
+
+    # input_arr= self.split("")
+    input_arr.each() do |char| #each with index
       if char.==("e") or char.==("E")
-        new_arr.push("3")
-      elsif char.==("o")or char.==("O")
+          new_arr.push("3")
+      elsif char == "o" || char == "O"
         new_arr.push("0")
+      elsif char.==("I")
+        new_arr.push("1")
+      elsif char.==("s") or char.==("S")
+        new_arr.push("z")
+      elsif char.==("~")
+        new_arr.push("s")
       else
         new_arr.push(char)
       end
--- leetspeak/spec/leetspeak_spec.rb
+++ leetspeak/spec/leetspeak_spec.rb
@@ -3,9 +3,15 @@ require('leetspeak')
 
 describe('String#leetspeak')do
   it("The letter 'e' should be replaced with 3.") do
-    expect(("streng").leetspeak()).to(eq("str3ng"))
+    expect(("treng").leetspeak()).to(eq("tr3ng"))
   end
   it("The letter 'o' should be replaced with 0.") do
-    expect(("strong").leetspeak()).to(eq("str0ng"))
+    expect(("trong").leetspeak()).to(eq("tr0ng"))
+  end
+  it("The letter 'I' should be replaced with 1.") do
+    expect(("trIng").leetspeak()).to(eq("tr1ng"))
+  end
+  it("The letter 's' should be replaced with 'z' unless it is the first letter of the word") do
+    expect(("strengs").leetspeak()).to(eq("str3ngz"))
   end
 end
